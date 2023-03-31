554f045a9a0652381a631b2a4374873dd18e5071
Matthias Reiseder <m.reiseder@outlook.com>
2023-03-31 20:32:52 +0200

    add example app

---
+++ MaterialDesignTreeViewBug/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+using System.Windows.Markup;
+
+namespace MaterialDesignTreeViewBug
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Fields
+
+        private readonly XmlLanguage de = XmlLanguage.GetLanguage("de-at");
+        private readonly XmlLanguage en = XmlLanguage.GetLanguage("en-us");
+
+        #endregion Fields
+
+        #region Constructors
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #endregion Constructors
+
+        #region Methods
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (Language == de)
+            {
+                Language = en;
+            }
+            else
+            {
+                Language = de;
+            }
+        }
+
+        #endregion Methods
+    }
+}
